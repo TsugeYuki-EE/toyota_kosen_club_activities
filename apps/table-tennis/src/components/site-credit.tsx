6e54c2023ac8fc7d90555e477ad8063333442bf7
tugeu <tugeu@example.com>
2026-04-04 22:28:13 +0900

    fix クレジット表記を更新

--- apps/table-tennis/src/components/site-credit.tsx
+++ apps/table-tennis/src/components/site-credit.tsx
@@ -5,10 +5,6 @@
       <a href="https://github.com/TsugeYuki-EE/toyota_kosen_club_activities" target="_blank" rel="noopener noreferrer">
         Yuki Tsuge
       </a>
-      . Hosted on{" "}
-      <a href="https://dashboard.render.com/" target="_blank" rel="noopener noreferrer">
-        Render
-      </a>
       . Source code available on{" "}
       <a href="https://github.com/TsugeYuki-EE/toyota_kosen_club_activities" target="_blank" rel="noopener noreferrer">
         GitHub
